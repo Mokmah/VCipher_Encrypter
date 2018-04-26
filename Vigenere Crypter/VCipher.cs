b408196247e483ea40f3808f989a22e69b19308d
William Garneau <willi_garn@hotmail.com>
2018-04-26 09:25:32 -0400

    Added vigenere cipher description

--- Vigenere Crypter/VCipher.cs
+++ Vigenere Crypter/VCipher.cs
@@ -4,6 +4,11 @@ using System.Text;
 /// <summary>
 /// Vigenere encryption and decryption functions
 /// By William Garneau
+/// 
+/// The Vigenère cipher is a method of encrypting alphabetic
+/// text by using a series of different Caesar ciphers based
+/// on the letters of a keyword. It is a simple form of
+/// polyalphabetic substitution. 
 /// </summary>
 /// 
 namespace Vigenere_Crypter
